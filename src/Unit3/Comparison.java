47609f4dcc661ca56a23dc6b96b5429764a6ded6
joker <842k40y5@gmail.com>
2025-01-10 05:07:38 +0800

    First commit

---
+++ src/Unit3/Comparison.java
@@ -0,0 +1,15 @@
+package Unit3;
+
+public class Comparison {
+
+	public static void main(String[] args) {
+		System.out.printf("10 > 5 結果 %b%n", 10 > 5);
+		System.out.printf("10 >= 5 結果 %b%n", 10 >= 5);
+		System.out.printf("10 < 5 結果 %b%n", 10 < 5);
+		System.out.printf("10 <= 5 結果 %b%n", 10 <= 5);
+		System.out.printf("10 == 5 結果 %b%n", 10 == 5);
+		System.out.printf("10 != 5 結果 %b%n", 10 != 5);
+
+	}
+
+}
