b58b995f2d8b2d74e506a0494b47a94b6fb58880
joker <842k40y5@gmail.com>
2025-01-11 06:29:53 +0800

    Update Unit3 Level5

---
+++ src/Unit3/Level4.java
@@ -0,0 +1,26 @@
+package Unit3;
+
+import java.util.Scanner;
+
+public class Level4 {
+
+	public static void main(String[] args) {
+		var scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+		var quotient = score / 10;
+		var level = switch (quotient) {
+		case 10, 9:
+			yield 'A';
+		case 8:
+			yield 'B';
+		case 7:
+			yield 'C';
+		case 6:
+			yield 'D';
+		default:
+			yield 'E';
+		};
+		System.out.printf("得分等級:%c%n", level);
+	}
+
+}
---
+++ src/Unit3/Level5.java
@@ -0,0 +1,25 @@
+package Unit3;
+
+import java.util.Scanner;
+
+public class Level5 {
+
+	public static void main(String[] args) {
+		final String warning = "(喔喔!不及格了!)";
+		var scanner = new Scanner(System.in);
+		int score = scanner.nextInt();
+		var quotient = score / 10;
+		var level = switch (quotient) {
+		case 10, 9 -> "A";
+		case 8 -> "B";
+		case 7 -> "C";
+		case 6 -> "D";
+		default -> {
+			String message = "E" + warning;
+			yield message;
+		}
+		};
+		System.out.printf("得分等級:%s%n", level);
+	}
+
+}
