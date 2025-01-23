2860513f17cdf905f3c094a580ac947a6ae1a5b4
joker <842k40y5@gmail.com>
2025-01-24 06:19:10 +0800

    Add Unit4 to DeepCopy

--- src/Unit4/DeepCopy.java
+++ src/Unit4/DeepCopy.java
@@ -6,12 +6,11 @@ public class DeepCopy {
 		Clothes3[] c1 = { new Clothes3("red", 'L'), new Clothes3("blue", 'L') };
 		var c2 = new Clothes3[c1.length];
 		for (int i = 0; i < c1.length; i++) {
-			c2[i] = new Clothes3(c1[i].color, c1[i].size); 
+			c2[i] = new Clothes3(c1[i].color, c1[i].size);
 		}
 		c1[0].color = "yellow";
 		System.out.println(c2[0].color);
 
-		
 	}
 
 }
