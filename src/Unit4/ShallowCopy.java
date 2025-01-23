b6aa7bc3e335218de71d99795706620a93efab0e
joker <842k40y5@gmail.com>
2025-01-24 02:43:18 +0800

    Add Unit4 to DeepCopy

--- src/Unit4/ShallowCopy.java
+++ src/Unit4/ShallowCopy.java
@@ -1,10 +1,26 @@
 package Unit4;
 
+class Clothes3 {
+	String color;
+	char size;
+
+	Clothes3(String color, char size) {
+		this.color = color;
+		this.size = size;
+	}
+
+}
+
 public class ShallowCopy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Clothes3[] c1 = {new Clothes3("red", 'L'),new Clothes3("blue", 'M')};
+		var c2 = new Clothes3[c1.length];
+		for(int i=0; i<c1.length; i++) {
+			c2[i] = c1[i];
+		}
+		c1[0].color = "yellow";
+		System.out.println(c2[0].color);
 	}
 
 }
